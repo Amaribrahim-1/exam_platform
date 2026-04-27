78790c8bb679b9929524b0e524bd0af87ac98ab1
Amaribrahim-1 <amaribrahimforwork1@gmail.com>
2026-04-28 02:11:09 +0300

    chore: secure supabase keys and update favicon Moved Supabase credentials to .env and updated .gitignore - Updated favicon to match sidebar icon with gradient styling - Changed site title in index.html to Exam.Io"

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://zejqsyzcgpjkcrecrujf.supabase.co";
-const supabaseKey = "sb_publishable_F2TQ78-LcdqFRLW07VGGhw_bPCRYKPM";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
